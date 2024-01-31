9ca2d63b4b7f1a97008c28e7f3d3e8b455ef13b1
Nick Johnson <nick@prospereducation.com>
2024-01-31 13:01:13 +1100

    Since the identity token audience does not match the client Id, we should not verify they match...

--- src/IdentityServer4/src/Validation/Default/TokenValidator.cs
+++ src/IdentityServer4/src/Validation/Default/TokenValidator.cs
@@ -101,7 +101,7 @@ namespace IdentityServer4.Validation
             _logger.LogDebug("Client found: {clientId} / {clientName}", client.ClientId, client.ClientName);
 
             var keys = await _keys.GetValidationKeysAsync();
-            var result = await ValidateJwtAsync(token, keys, audience: clientId, validateLifetime: validateLifetime);
+            var result = await ValidateJwtAsync(token, keys, validateLifetime: validateLifetime);
 
             result.Client = client;
 
